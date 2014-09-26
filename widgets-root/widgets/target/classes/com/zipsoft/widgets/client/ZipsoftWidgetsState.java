fa31e182688327faf8ca33559395a6cc2c8cd9f8
markoradinovic <markoradinovic79@gmail.com>
2014-09-26 16:41:45 +0200

    fix repo

--- widgets-root/widgets/target/classes/com/zipsoft/widgets/client/ZipsoftWidgetsState.java
+++
@@ -1,9 +0,0 @@
-package com.zipsoft.widgets.client;
-
-public class ZipsoftWidgetsState extends com.vaadin.shared.AbstractComponentState {
-
-	private static final long serialVersionUID = 8260906592607885301L;
-	// State can have both public variable and bean properties
-	public String text = "ZipsoftWidgets";
-
-}
--- widgets-root/widgets/target/classes/com/zipsoft/widgets/client/expandingtextarea/ExpandingTextAreaServerRpc.java
+++
@@ -1,9 +0,0 @@
-package com.zipsoft.widgets.client.expandingtextarea;
-
-import com.vaadin.shared.communication.ServerRpc;
-
-public interface ExpandingTextAreaServerRpc extends ServerRpc {
-	
-	void setRows(int rows);
-
-}
